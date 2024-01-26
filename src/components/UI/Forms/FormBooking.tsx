f6f345fbb8c34d588e2ab429fb209c2cd37ea46e
Oleksii-Kriuckov <75043251+Oleksii-Kriuckov@users.noreply.github.com>
2024-01-26 03:04:24 +0200

    fix merge contradictions

--- src/components/UI/Forms/FormBooking.tsx
+++ src/components/UI/Forms/FormBooking.tsx
@@ -1,34 +1,18 @@
 import React from "react";
-<<<<<<< HEAD
 import { Button, Form, Input, Select, InputNumber } from "antd";
 import codes from "../../../assets/codes.json";
-=======
-import { Button, Input, Form } from "antd"; // https://ant.design/components/form
 import "./styles/style.css";
->>>>>>> e86ed68e95afe78295ef9af68edce48ae7ce526b
 import "./styles/adaptive.css";
-import "./styles/style.css";
+// https://ant.design/components/form
 
 const { Option } = Select;
 
-<<<<<<< HEAD
 export const FormBooking : React.FC = () => {
-=======
-const FormBooking = (props: Props) => {
->>>>>>> e86ed68e95afe78295ef9af68edce48ae7ce526b
   const [form] = Form.useForm();
 
   const onFinish = (values: any) => {
     console.log("Received values of form: ", values);
   };
-<<<<<<< HEAD
-=======
-
-  // const onSubmit = (event: React.FormEvent<HTMLButtonElement>) => {
-  //   event.preventDefault();
-  // };
-  // const onClick = () => onSubmit;
->>>>>>> e86ed68e95afe78295ef9af68edce48ae7ce526b
 
   const prefixSelector = (
     <Form.Item name="prefix" noStyle>
@@ -42,7 +26,6 @@ const FormBooking = (props: Props) => {
 
   return (
     <Form
-<<<<<<< HEAD
       name="form_booking"
       className="form d-flex flex-column flex-lg-row align-items-lg-center"
       form={form}
@@ -56,23 +39,11 @@ const FormBooking = (props: Props) => {
       <div className="inputs_wrap d-flex flex-column flex-md-row">
         <Form.Item
           name="name"
-=======
-      id="form_booking"
-      className="form d-flex flex-column flex-lg-row "
-      layout="vertical"
-      form={form}
-      onFinish={onFinish}
-      // variant="outlined"
-      autoComplete="off"
-    >
-      <div className="inputs_wrap d-flex flex-column flex-md-row">
-        <Form.Item
->>>>>>> e86ed68e95afe78295ef9af68edce48ae7ce526b
+
           label="Name"
           className="input_block input_block_booking"
           rules={[{ required: true, message: "Please input your name!" }]}
         >
-<<<<<<< HEAD
           <Input size="large" placeholder="John Smith" />
         </Form.Item>
 
@@ -87,19 +58,6 @@ const FormBooking = (props: Props) => {
         >
           <InputNumber
             minLength={7}
-=======
-          {/* <label htmlFor="name">Name</label> */}
-          <Input size="large" name="name" id="name" placeholder="John Smith" />
-        </Form.Item>
-
-        <Form.Item
-          label="Phone"
-          className="input_block input_block_booking"
-          // rules={[{ type: "" }]}
-        >
-          {/* <label htmlFor="phone">Phone</label> */}
-          <Input
->>>>>>> e86ed68e95afe78295ef9af68edce48ae7ce526b
             size="large"
             addonBefore={prefixSelector}
             style={{ width: "100%" }}
@@ -108,7 +66,6 @@ const FormBooking = (props: Props) => {
         </Form.Item>
 
         <Form.Item
-<<<<<<< HEAD
           name="email"
           label="E-mail"
           className="input_block input_block_booking"
@@ -124,23 +81,6 @@ const FormBooking = (props: Props) => {
           ]}
         >
           <Input size="large" placeholder="jsmith@gmail.com" />
-=======
-          className="input_block input_block_booking"
-          label="Email"
-          rules={[
-            { type: "email", message: "The input is not valid E-mail!" },
-            { required: true, message: "Please input your E-mail!" },
-          ]}
-        >
-          {/* <label htmlFor="email">E-mail</label> */}
-          <Input
-            size="large"
-            type="email"
-            name="email"
-            id="email"
-            placeholder="jsmith@gmail.com"
-          />
->>>>>>> e86ed68e95afe78295ef9af68edce48ae7ce526b
         </Form.Item>
       </div>
 
@@ -155,5 +95,3 @@ const FormBooking = (props: Props) => {
     </Form>
   );
 };
-
-
