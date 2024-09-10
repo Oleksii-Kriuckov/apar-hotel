75686abffc2beb107d2e50663f085c808aef9b7a
Oleksii <lexx2109@ukr.net>
2024-09-10 21:45:32 +0300

    change info file

--- src/assets/Info.ts
+++ src/assets/Info.ts
@@ -24,68 +24,68 @@ export const allHotels: IHotelsInCity[] = [
     hotelsInfo: [
       {
         hotelName: "Arena-Summit",
-        // address: "bul. of Lesia Ukrayinka 3",
-        address: "бул. Лесі Українки, 3",
+        address: "bul. of Lesia Ukrayinka 3",
+        // address: "бул. Лесі Українки, 3",
         tel: "+380996619218",
         email: "arena-summit@apart-hotel.com",
         coordinates: { lat: 50.43293422168239, lng: 30.53353608991025 },
         conveniences: [
-          "Зручне розташування",
-          "Парковка авто",
-          "Комфортні ліжка та подушки з білосніжною білизною",
-          "Зелена зона",
-          "Пральня, кімната зберігання багажу, автомати з продажу кави та снеків",
-          "Можемо прийняти до 26 осіб",
+          "Convenient location",
+          "Car parking",
+          "Comfortable beds and pillows with snow-white linen",
+          "Green area",
+          "Laundry service, luggage storage, coffee snack vending machines",
+          "up to 26 people",
         ],
-        // description: `Arena Summit Apart Hotel is suitable for both families and business trips. 
-        // Upon request, accommodation in family rooms and non-smoking rooms. The location in the city center. 
+        description: `Arena Summit Apart Hotel is suitable for both families and business trips. 
+        Upon request, accommodation in family rooms and non-smoking rooms. The location in the city center. 
 
-        //   The hotel is very comfortable and cozy, the interior is made in a modern style. 
-        //   Absolutely all furniture was designed with maximum convenience for guests. All rooms equipped with mini-kitchenettes for a comfortable long stay. 
-        //   24/7 reception and excellent service, including free Wi-Fi (in rooms and public areas), luggage storage, safe, laundry room, room service and fire alarm system are created for a comfortable stay of our guests.
+          The hotel is very comfortable and cozy, the interior is made in a modern style. 
+          Absolutely all furniture was designed with maximum convenience for guests. All rooms equipped with mini-kitchenettes for a comfortable long stay. 
+          24/7 reception and excellent service, including free Wi-Fi (in rooms and public areas), luggage storage, safe, laundry room, room service and fire alarm system are created for a comfortable stay of our guests.
           
-        //   The upper floors can be easily reached by elevator or stairs. Those arriving with their own car can use the garage or parking lot. Shuttle bus services are available for a fee.`,
-        description: `Готель Arena Summit підійде як для сімейного відпочинку, так і для бізнес подорожей. Розташований в центі міста.
-        За запитом розміщення в сімейних номерах та номерах для некурців.
-        
-        Готель дуже комфортний та затишний, інтер'єр зроблений в сучасному стилі.
-        Абсолютно всі меблі розроблені з максимальною зручністю для гостей. Більшість кімнат оснащені власними міні-кухнями для довгого комфортного перебування.
-        Цілодобовий ресепшн та бездоганий сервіс, включаючи безкоштовний Wi-Fi (в кімнатах та публічних місцях), кімната зберігання багажу, сейф, пральня, обслуговування номерів та протипожежна сигналізація зроблять комфортним перебування для наших гостей.
-        
-        На верхні поверхи можна легко піднятися на ліфті або сходами. Ті, хто приїжджає на власному автомобілі, можуть скористатися гаражем або автостоянкою. За окрему плату надаються послуги трансферу до аеропорту.`,
+          The upper floors can be easily reached by elevator or stairs. Those arriving with their own car can use the garage or parking lot. Shuttle bus services are available for a fee.`,
+        // description: `Готель Arena Summit підійде як для сімейного відпочинку, так і для бізнес подорожей. Розташований в центі міста.
+        // За запитом розміщення в сімейних номерах та номерах для некурців.
+
+        // Готель дуже комфортний та затишний, інтер'єр зроблений в сучасному стилі.
+        // Абсолютно всі меблі розроблені з максимальною зручністю для гостей. Більшість кімнат оснащені власними міні-кухнями для довгого комфортного перебування.
+        // Цілодобовий ресепшн та бездоганий сервіс, включаючи безкоштовний Wi-Fi (в кімнатах та публічних місцях), кімната зберігання багажу, сейф, пральня, обслуговування номерів та протипожежна сигналізація зроблять комфортним перебування для наших гостей.
+
+        // На верхні поверхи можна легко піднятися на ліфті або сходами. Ті, хто приїжджає на власному автомобілі, можуть скористатися гаражем або автостоянкою. За окрему плату надаються послуги трансферу до аеропорту.`,
         images: {
           forSlider: [ArenaSummitRoom22V2, ArenaSummitRoom32V3, ArenaSummitBathroom24],
         },
       },
       {
         hotelName: "Tiso",
-        address: "вул. Жилянська, 59",
-        // address: "Zhylianska, street 59",
+        // address: "вул. Жилянська, 59",
+        address: "Zhylianska, street 59",
         tel: "+380952805220",
         email: "tiso@apart-hotel.com",
         coordinates: { lat: 50.4545998769799, lng: 30.520113578857607 },
         conveniences: [
-          "Розташування - центр міста",
-          "Підземна парковка",
-          "Поруч знакові пам'ятки",
-          "Затишні номери",
-          "2 ресторани та замовлення їжі в номер",
-          "В зоні загального користування повна кухня, пральна машина, праска, загалом все як вдома",
-          "Готові прийняти до 24 осіб",
+          "Location - city center",
+          "Underground parking",
+          "Near iconic sights",
+          "Cosy rooms",
+          "2 restaurants and ordering food to the room",
+          "The common area has a full kitchen, a washing machine, an iron, in general, everything is like home.",
+          "up to 24 people",
         ],
-        // description: `Our hotel very harmoniously combines all the necessary requirements for living of a modern person. The rooms are apartment format, but with hotel service and security. 
+        description: `Our hotel very harmoniously combines all the necessary requirements for living of a modern person. The rooms are apartment format, but with hotel service and security. 
         
-        // Each room at Tiso ApartHotel includes bright décor, an air conditioner, a minibar, a flat-screen TV, and bathrooms come with a hairdryer. Among other amenities, all rooms have WiFi (free of charge). Non-smoking rooms are available. The elevator will take you to the upper floors. A safe, 24-hour security, room service and laundry are not a complete list of amenities at this hotel. WiFi in public areas.
+        Each room at Tiso ApartHotel includes bright décor, an air conditioner, a minibar, a flat-screen TV, and bathrooms come with a hairdryer. Among other amenities, all rooms have WiFi (free of charge). Non-smoking rooms are available. The elevator will take you to the upper floors. A safe, 24-hour security, room service and laundry are not a complete list of amenities at this hotel. WiFi in public areas.
 
-        // Restaurants can be found on the ground and 1st floors of the residential estate and several dining options are also available within a 5-minute walk of the property.
+        Restaurants can be found on the ground and 1st floors of the residential estate and several dining options are also available within a 5-minute walk of the property.
 
-        // Our hotel has its own parking lot. Shuttle bus services to Boryspil Airport are available for a fee.`,
-        description: `Наш готель дуже гармонійно поєднує в собі всі необхідні вимоги для проживання сучасної людини. Номери квартирного формату, але з готельним сервісом та охороною.
-        
-        Усі номери апарт-готелю Tiso оформлені в яскравих тонах і оснащені кондиціонером, міні-баром, телевізором із плоским екраном і ванною кімнатою з феном. Серед інших зручностей у всіх номерах є Wi-Fi (безкоштовно). Є номери для некурців. На верхні поверхи вас підніме ліфт. Сейф, цілодобова охорона, обслуговування номерів і пральня - далеко не повний перелік зручностей цього готелю. WiFi в громадських місцях.
-        
-        На першому та другому поверхах готельного комплексу працюють ресторани, а в межах 5 хвилин ходьби від закладу працюють кілька закладів харчування.
-        Наш готель має власну автостоянку. Трансфер до аеропорту Бориспіль надається за додаткову плату.`,
+        Our hotel has its own parking lot. Shuttle bus services to Boryspil Airport are available for a fee.`,
+        // description: `Наш готель дуже гармонійно поєднує в собі всі необхідні вимоги для проживання сучасної людини. Номери квартирного формату, але з готельним сервісом та охороною.
+
+        // Усі номери апарт-готелю Tiso оформлені в яскравих тонах і оснащені кондиціонером, міні-баром, телевізором із плоским екраном і ванною кімнатою з феном. Серед інших зручностей у всіх номерах є Wi-Fi (безкоштовно). Є номери для некурців. На верхні поверхи вас підніме ліфт. Сейф, цілодобова охорона, обслуговування номерів і пральня - далеко не повний перелік зручностей цього готелю. WiFi в громадських місцях.
+
+        // На першому та другому поверхах готельного комплексу працюють ресторани, а в межах 5 хвилин ходьби від закладу працюють кілька закладів харчування.
+        // Наш готель має власну автостоянку. Трансфер до аеропорту Бориспіль надається за додаткову плату.`,
         images: {
           forSlider: [TiSORoom43, TisoBath, TisoRestaurant],
         },
@@ -98,37 +98,29 @@ export const allHotels: IHotelsInCity[] = [
     hotelsInfo: [
       {
         hotelName: "Code-10",
-        address: "вул. Богдана Хмельницького, 157",
-        // address: "street of Bohdan Khmelnytskyi, 157",
+        address: "street of Bohdan Khmelnytskyi, 157",
+        // address: "вул. Богдана Хмельницького, 157",
         tel: "+380682975027",
         email: "code-10@apart-hotel.com",
         coordinates: { lat: 49.85410453856136, lng: 24.043041132804692 },
         conveniences: [
-          "Зручне розташування",
-          "Парковка авто",
-          "Близько торгрвельні центри, продуктові магазини",
-          "Затишне, тихе місце",
-          "Пральня, кімната зберігання багажу, автомати з продажу кави та снеків",
-          "Можемо прийняти до 30 людей",
+          "Convenient location",
+          "Car parking",
+          "Close to malls, grocery stores",
+          "Quiet place",
+          "Laundry service, luggage storage, coffee snack vending machines",
+          "up to 30 people",
         ],
-        // description: `Apart Hotel Code 10 located near the city center. Free Wifi is featured throughout the property. 
-        // Family rooms and non-smoking rooms are available upon request. 
-        // Some rooms are allergy-free and soundproof, and certain ones have a terrace.
+        description: `Apart Hotel Code 10 located near the city center. Free Wifi is featured throughout the property. 
+        Family rooms and non-smoking rooms are available upon request. 
+        Some rooms are allergy-free and soundproof, and certain ones have a terrace.
         
-        // All rooms have a desk, a flat-screen TV, an air conditioner, a private bathroom and towels. 
-        // The various facilities at this apartment hotel - such as luggage storage, concierge service, transfer and meeting facilities - will make your stay especially comfortable.
+        All rooms have a desk, a flat-screen TV, an air conditioner, a private bathroom and towels. 
+        The various facilities at this apartment hotel - such as luggage storage, concierge service, transfer and meeting facilities - will make your stay especially comfortable.
 
-        // Walking through the hotel park will improve the mood. You can park your car in the hotel's own car park. 
+        Walking through the hotel park will improve the mood. You can park your car in the hotel's own car park. 
         
-        // The staff of the tourist office will be happy to help you with the organization of tours and excursions.`,
-        description: `Готель Code 10 розташований біля центру міста. На всій території закладу надається безкоштовний Wi-Fi.
-        За вимогою надаються сімейні номери та номери для некурців. Деякі номери гіпоалергенні та звуконепроникні, а деякі мають терасу або балкон.
-        
-        У всіх номерах є стіл та стільці, смарт телевізор, кондиціонер, окрема ванна кімната з феном та рушниками.
-        Різноманітні зручності в цьому апарт-готелі, такі як камера зберігання багажу, послуги консьєржа, трансфер і приміщення для проведення засідань, зроблять ваше перебування особливо комфортним.
-        
-        Прогулянка парком готелю покращить настрій. Ви можете залишити свій автомобіль на власній автостоянці готелю.
-        Працівники туристичного бюро з радістю допоможуть Вам з організацією турів та екскурсій.`,
+        The staff of the tourist office will be happy to help you with the organization of tours and excursions.`,
         images: {
           forSlider: [Code10Room21, Code10Room31, Code10Bath],
         },
@@ -144,8 +136,10 @@ export const info_en = {
     p: "We are waiting for you to visit, we are ready to receive you at anytime of the day.",
     btn: "CONNECT WITH US",
   },
-  mainWarningMessage: `This site is presented as a demonstration of the author's own work. You CANNOT actually book hotel rooms through this site.
- Any information presented on this site may be inaccurate. `
+  mainWarningMessage: `This site is presented as a demonstration of the author's own work. 
+  You CANNOT actually book hotel rooms through this site. 
+  Instead, the real business logic of booking is implemented here.
+  Any information presented on this site may be inaccurate. `
 };
 
 export const info_ua = {
